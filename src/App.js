33c35a1feb8521a621623650ff9a235e35293cf3
Tony Thai Do <tony.thai.do@sykorait.com>
2023-12-03 20:32:37 +0100

    init layeout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import * as Slider from '@radix-ui/react-slider'
 
-function App() {
+
+const App = () => {
+  const [value, setValue] = useState([300]);
+  const handleChange = (event) => {
+    setValue(value);
+    console.log(`Slider value: ${value}`)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ position: 'absolute', top: 0, left: 0 }}>
+        {/* Replace this with your slider component */}
+        <Slider.Root className="SliderRoot" value={value} onValueChange={handleChange} max={100} step={1}>
+          <Slider.Track className="SliderTrack">
+            <Slider.Range className="SliderRange" />
+          </Slider.Track>
+          <Slider.Thumb className="SliderThumb" aria-label="Volume" />
+        </Slider.Root>
+        <p>Slider goes here</p>
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '150px' }}>
+        <p>Your text paragraph goes here</p>
+      </div>
+      <div style={{ position: 'absolute', bottom: 100, width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
+        <button>Button 1</button>
+        <button>Button 2</button>
+        <button>Button 3</button>
+      </div>
     </div>
   );
 }
